5d2e7eb4fb104b6d58ae5e9881a32bfb2f3e86d1
minnica <minnica20@gmail.com>
2025-04-10 22:13:12 -0600

    feat(*): instructions for exercice three

---
+++ list-names.js
@@ -0,0 +1,14 @@
+// Crea una aplicación con un botón, una lista y un input text.
+// En el input text, el usuario podrá ingregar cualquier nombre y dicho nombre tendrá que agregarse a una lista desordenada
+// Solo puedes usar un método y una propiedad
+
+import { LitElement, html } from "lit";
+
+export class listNames extends LitElement {
+  static get properties() {}
+
+  constructor() {}
+
+  render() {}
+}
+customElements.define("list-names", listNames);
